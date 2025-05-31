296e2b1ecd6f19fe5515dbe14223c780180e95ee
David <45599491+Daviiks@users.noreply.github.com>
2025-05-31 21:12:15 +0600

    Add files via upload

---
+++ backend/users/apps.py
@@ -0,0 +1,5 @@
+﻿from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    name = 'users'
---
+++ backend/users/serializers.py
@@ -0,0 +1,36 @@
+﻿from rest_framework import serializers
+from recipes.models import Follow
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer для чтения / создания пользователя модели User.
+    """
+
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "password",
+            "is_subscribed",
+        )
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "is_subscribed": {"read_only": True},
+        }
+
+    def get_is_subscribed(self, obj):
+        user = self.context.get("request").user
+        if not user.is_anonymous:
+            return Follow.objects.filter(user=user, author=obj).exists()
+        return False
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
